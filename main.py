941170290dd632bd9340765a23904e33bbaea6ad
Thierry Wolffenbuttel <45482569+thierrywo@users.noreply.github.com>
2019-06-21 09:37:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #dit is ons project
 
+
 def start_text():
   print("\nThe Year is 1952. The axis powers have taken over over almost the entire world. Also the continental US has fallen. The only place that is still under the rule of the US army and navy, is Alcatraz. This orison has been fitted with the most advanced weapons of the time. You are a US marine, you wake up in the old cellhouses, which have been transformed into an underground bunker with sleeping facilities. \n ")
 
-
+inventory = []
 directions = []
-rooms = ["x", "x", "x", ["church", "This is an old church", "knive"], ["parade ground", "Where the army has it's parades"], ["cellhouse", "Here are the old cells"], ["ruins of wardens house", "This is a room"], ["restrooms", "This is a room"], ["recreation yard", "This is a room"], ["Army ground", "This is a room", "Binoculars"], ["cummunication center", "This is a room"], ["dock", "This is a room"], ["pool", "This is a room", "torch"], ["trainingscamp", "This is a room"], ["boat", "This is a room"], "x", "x", "x"]
+rooms = ["x", "x", "x", ["church", "In this church the old prisoners of fort Alcatraz", "knife"], ["parade ground", "Since this is the last base of the US army, the have here their parades"], ["cellhouse", "In fort Alcatraz there where prisoners. This is the place where they lived"], ["ruins of wardens house", "Here were the prison guards"], ["restrooms", "You now what they do here ;)"], ["recreation yard", "In the past this was the place where the prisoners had their free time. Nowadays it is a place for soldiers in their spare time"], ["Army ground", "Here does the army has its parades", "binoculars"], ["cummunication center", "To make sure the communication of all the troops of the US military is good, they built this building", "Telephone"], ["dock", "This dock houses all the weaponry of the us navy."], ["pool", "Here the soldiers go to unwind.", "torch"], ["trainingscamp", "Here, the soldiers train for 8 hours a day.", "gun"], ["boat", "Here, the soldiers go out for reconnaissance"], "x", "x", "x"]
  
 
 
@@ -13,7 +14,8 @@ directions = {
   "e"or"E"  : "east",
   "s"or"S"  : "south",
   "w"or"W"  : "west",
-  "n"or"N"  : "north"
+  "n"or"N"  : "north",
+  "g"or"G"  : "get"
 }
 
 roomnumber = 5
@@ -41,32 +43,45 @@ while True:
   if not (roomnumber - 2) % 3 == 0:
     directions.append("e")
 
-  print("You can go to:")
+  if len(rooms[roomnumber]) == 3:
+   directions.append("g")
+   weapon = rooms[roomnumber][2]
+
+
+  print("You can choose:")
   print(directions)
 
   chosendirection = input("enter your way: ")
 
   if chosendirection == "q":
+    print("\nWhy are you leaving us %s (" % name)
     print("exiting")
     raise SystemExit(0)
 
   if chosendirection == "r":
     print("restarting")
-    
+  
+  
+  
+  print(chosendirection)
+  
+  if chosendirection == "g" or chosendirection == "G":
+    inventory.append(weapon)
+    print("You got the %s" %weapon)
+    del rooms[roomnumber][2]
 
   if (chosendirection not in directions) and (chosendirection.lower() not in directions):
-  
-    if chosendirection == "w" or chosendirection == "W":
-      print("\nyou can not go west\n")
-    elif chosendirection == "n" or chosendirection == "N":
+   if chosendirection == "w" or chosendirection == "W":
+     print("\nyou can not go west\n")
+   elif chosendirection == "n" or chosendirection == "N":
       print("\nyou can not go north\n")
-    elif chosendirection == "s" or chosendirection == "S":
-      print("\nyou can not go south\n")
-    elif chosendirection == "e" or chosendirection == "E":
-      print("\nyou can not go east\n")
-    else:
+   elif chosendirection == "s" or chosendirection == "S":
+    print("\nyou can not go south\n")
+   elif chosendirection == "e" or chosendirection == "E":
+     print("\nyou can not go east\n")
+   else:
       print("\nThis direction is not available\n")
-    continue
+      continue
         
   if chosendirection == "w" or chosendirection == "W":
     roomnumber = roomnumber - 1
@@ -86,3 +101,8 @@ while True:
 
   text = name + (", you have reached the %s. " % rooms[roomnumber][0]) + (rooms[roomnumber][1])
   print(text)
+
+  print("\nYou have %s in inventory" % inventory)
+
+  if len(rooms[roomnumber]) == 3:
+    print("\nYou see: %s" % rooms[roomnumber][2])
